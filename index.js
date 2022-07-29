085814cc84262acb13fb3ae871b13725662e60b7
sajo-dev <github@sajo.com.br>
2022-07-28 22:57:26 -0300

    Init

---
+++ index.js
@@ -0,0 +1,33 @@
+scanButton.addEventListener("click", async () => {
+  log("User clicked scan button");
+
+  try {
+    const ndef = new NDEFReader();
+    await ndef.scan();
+    log("> Scan started");
+
+    ndef.addEventListener("readingerror", () => {
+      log("Argh! Cannot read data from the NFC tag. Try another one?");
+    });
+
+    ndef.addEventListener("reading", ({ message, serialNumber }) => {
+      log(`> Serial Number: ${serialNumber}`);
+      log(`> Records: (${message.records.length})`);
+    });
+  } catch (error) {
+    log("Argh! " + error);
+  }
+});
+
+writeButton.addEventListener("click", async () => {
+  log("User clicked write button");
+
+  try {
+    const ndef = new NDEFReader();
+    let person = prompt("Insira o valor a ser gravado:", "");
+    await ndef.write("Hello world!");
+    log("> Message written");
+  } catch (error) {
+    log("Argh! " + error);
+  }
+});
